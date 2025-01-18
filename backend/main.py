2efa59d25fcb6a778a468621d122b90e50e61088
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2025-01-18 11:45:25 +0800

    Remove debug message

--- backend/main.py
+++ backend/main.py
@@ -8,11 +8,6 @@ from snowflake import SnowflakeID
 
 async def main():
     await setup()
-
-    user = User(name="123", password=b"123")
-    print(SnowflakeID)
-    # await User.insert(user)
-
     await run_api()
 
 if __name__ == "__main__":
